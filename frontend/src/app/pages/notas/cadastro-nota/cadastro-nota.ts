b2ab719305410c4df4158949c217951bafb6966c
Adriano Souza <adrianoads910@gmail.com>
2025-11-09 13:24:45 -0300

    frontend estructure

---
+++ frontend/src/app/pages/notas/cadastro-nota/cadastro-nota.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-nota',
+  imports: [],
+  templateUrl: './cadastro-nota.html',
+  styleUrl: './cadastro-nota.scss',
+})
+export class CadastroNota {
+
+}

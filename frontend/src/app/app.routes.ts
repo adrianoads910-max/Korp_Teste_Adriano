5f2c73be2dceb683c9be5d0cbb98eac0a783483b
Adriano Souza <adrianoads910@gmail.com>
2025-11-09 13:56:55 -0300

    router link create

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'produtos/listar',
+    pathMatch: 'full'
+  },
+
+  // ✅ ROTAS DE PRODUTOS
+  {
+    path: 'produtos/cadastrar',
+    loadComponent: () =>
+      import('./pages/produtos/cadastro-produto/cadastro-produto')
+        .then(m => m.CadastroProdutoComponent)
+  },
+  {
+    path: 'produtos/listar',
+    loadComponent: () =>
+      import('./pages/produtos/lista-produtos/lista-produtos')
+        .then(m => m.ListaProdutosComponent)
+  },
+
+  // ✅ ROTAS DE NOTAS
+  {
+    path: 'notas/cadastrar',
+    loadComponent: () =>
+      import('./pages/notas/cadastro-nota/cadastro-nota')
+        .then(m => m.CadastroNotaComponent)
+  },
+  {
+    path: 'notas/listar',
+    loadComponent: () =>
+      import('./pages/notas/lista-notas/lista-notas')
+        .then(m => m.ListaNotasComponent)
+  },
+];
